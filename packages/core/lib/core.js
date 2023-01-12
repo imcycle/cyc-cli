ca0a33dbf52fc9ca9be239683d1c9987e5098db4
caoyucong <whosmeya@gmail.com>
2023-01-12 12:02:44 +0800

    first commit

---
+++ packages/core/lib/core.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const {log} = require('@cyc/utils');
+
+module.exports = core;
+
+async function core() {
+    try {
+        await prepare();
+    } catch(e) {
+        log.error(e.message)
+    }
+}
+
+async function prepare() {
+
+}
