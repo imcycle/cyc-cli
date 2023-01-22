f8c4e522e73e61f009f5f1d662438ffcbada05b2
caoyucong <whosmeya@gmail.com>
2023-01-23 00:55:19 +0800

    feat: add init command

--- packages/core/lib/core.js
+++ packages/core/lib/core.js
@@ -1,18 +1,35 @@
-'use strict';
+"use strict";
 
-const {log} = require('@cyc/utils');
+const { program } = require("commander");
+const packageConfig = require("../package");
+
+const { log } = require("@cyc-cli/utils");
+const init = require("@cyc-cli/init");
 
 module.exports = core;
 
 async function core() {
-    // TODO..
-    try {
-        await prepare();
-    } catch(e) {
-        log.error(e.message)
-    }
+  try {
+    prepare();
+    registerCommand();
+  } catch (e) {
+    log.error(e.message);
+  }
 }
 
-async function prepare() {
+function prepare() {}
 
+function registerCommand() {
+  program.version(packageConfig.version).usage("<command> [options]");
+
+  program
+    .command("init [type]")
+    .description("项目初始化")
+    .option("--packagePath <packagePath>", "手动指定init包路径")
+    .option("--force", "强制安装")
+    .action((type, { packagePath, force }) => {
+      init();
+    });
+
+  program.parse(process.argv);
 }
